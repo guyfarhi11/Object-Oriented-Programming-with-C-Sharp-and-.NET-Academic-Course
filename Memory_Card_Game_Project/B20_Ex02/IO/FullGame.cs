6fdffa794e4d7de0d86c46ce6d841d101e7d0e57
Guy Farhi <guyfarhi11@gmail.com>
2020-07-14 19:02:17 +0300

    Add files via upload

---
+++ Memory_Card_Game_Project/B20_Ex02/IO/FullGame.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using B20_Ex02.IO;
+
+namespace B20_Ex02.GameLogic
+{
+    internal class FullGame
+    {
+        internal static void RunFullGame()
+        {
+            GraphicLayer game = new GraphicLayer();
+            while (game.RunGame())
+            {
+                Console.Clear();
+                game = new GraphicLayer();
+            }
+        }
+    }
+}
---
+++ Memory_Card_Game_Project/B20_Ex02/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace B20_Ex02
+{
+    public class Program
+    {
+        public static void Main(string[] i_Args)
+        {
+            GameLogic.FullGame.RunFullGame();
+        }
+    }
+}
